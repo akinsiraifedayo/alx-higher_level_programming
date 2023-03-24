586d93d3bf9bd174fae774f68ede829e7095651e
akinsiraifedayo <akinsiraolympicson@gmail.com>
2023-03-24 23:28:25 +0100

    edit city model

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,16 +2,34 @@
 """
 Lists all states from the database hbtn_0e_0_usa
 """
-import sys
+
 import MySQLdb
+import sys
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+    # Database connection arguments
+    host = 'localhost'
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states;")
-    states = cur.fetchall()
+    # Connect to MySQL database
+    db_conn = MySQLdb.connect(host=host, port=port, user=user,
+                              passwd=passwd, db=db)
 
-    for state in states:
-        print(state)
+    # Create cursor to execute queries
+    cursor = db_conn.cursor()
+
+    # Execute query to fetch all states from the table states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all rows and print them
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db_conn.close()
+
